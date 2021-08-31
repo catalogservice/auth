fbd235d3d53cf586dd5bad834600bc09fa0a40fc
Anthony Finix <anthonyfinix@gmail.com>
2021-08-31 22:29:06 +0530

    added jwt token and user/me now working

--- src/user/jwt.service.ts
+++ src/user/jwt.service.ts
@@ -1,6 +1,19 @@
 import { Injectable } from "@nestjs/common";
+const jwt = require('jsonwebtoken');
 
 @Injectable()
 export class JwtService {
+    readonly key = "secret";
+
+    sign(payload){
+        return jwt.sign(payload,this.key)
+    }
+
+    verify(token){
+        return jwt.verify(token,this.key)
+    }
     
+    decode(token){
+        return jwt.decode(token)
+    }
 }
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,20 +1,31 @@
-import { Body, Controller, Inject, Post } from "@nestjs/common";
+import { Body, Controller, Get, Post, Req, Res } from "@nestjs/common";
+import { Request, Response } from "express";
 import { BcryptService } from "./bcrypt.service";
 import { LoginDto } from "./dto/loginDto";
 import { RegisterDto } from "./dto/registerDto";
+import { JwtService } from "./jwt.service";
 import { UserService } from "./user.service";
 
 @Controller('user')
 export class UserController {
+
     constructor(
         private userService: UserService,
-        private bcryptService: BcryptService
+        private bcryptService: BcryptService,
+        private jwtService: JwtService
     ) { }
+
     @Post('/login')
-    async login(@Body() body: LoginDto) {
-        let response = await this.userService.getUserByUsername(body.username);
-        if (!response.data) return 'no response';
-        return response.data;
+    async login(@Body() body: LoginDto, @Res() res: Response) {
+        let dbUserResponse = await this.userService.getUserByUsername(body.username);
+        if (!dbUserResponse.data) return res.send('no response');
+        let { password } = dbUserResponse.data;
+        if (!this.bcryptService.compare(body.password, password)) return res.send('password does not match');
+        let userPayload: any = { username: dbUserResponse.data.username, first_name: dbUserResponse.data.first_name, last_name: dbUserResponse.data.last_name }
+        if (dbUserResponse.data.middle_name) userPayload.middle_name = dbUserResponse.data.middle_name;
+        let token = this.jwtService.sign(userPayload)
+        res.setHeader('Authorization', `Bearer ${token}`);
+        return res.json(userPayload)
     }
 
     @Post('/register')
@@ -22,9 +33,22 @@ export class UserController {
         let response = await this.userService.getUserByUsername(body.username);
         if (!response.data) return 'no response';
         if (response.data._id) return 'user already exists';
-        let newUser:any = body;
+        let newUser: any = body;
         newUser.password = this.bcryptService.hash(body.password)
         this.userService.createUser(newUser)
         return 'success';
+    }
+
+    @Get('/me')
+    async getCurrentUser(@Req() req: Request) {
+        let authorization = req.headers['authorization'];
+        if(!authorization) return "no authorization found";
+        try{
+            let jwtTokenPayload = this.jwtService.verify(authorization.split(' ')[1]);
+            let response = await this.userService.getUserByUsername(jwtTokenPayload.username);
+            return response.data;
+        }catch(e){
+            return e.message
+        }
     }
 }
